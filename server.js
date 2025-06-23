056e2e29fc9b031385eab85a1a6bdf39a7496fc4
LJAMINE <aminelmgrmj@gmail.com>
2025-06-23 12:01:43 +0100

    final of 4th video

--- server.js
+++ server.js
@@ -9,12 +9,26 @@ const server = http.createServer((req, res) => {
   switch (req.url) {
     case "/":
       path += "index.html";
+      res.statusCode = 200; // OK
       break;
     case "/about":
       path += "about.html";
+      res.statusCode = 200; // OK
+
       break;
+
+        case "/about-me":
+
+       res.statusCode = 301; // OK
+       res.setHeader("Location", "/about");
+       res.end();
+
+      break;
+
     default:
       path += "404.html";
+      res.statusCode = 404;
+
       break;
   }
 
